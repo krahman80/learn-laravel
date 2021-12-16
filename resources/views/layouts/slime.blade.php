ae13b833b7becef6adc1f2af1d6062245e66624a
krahman80 <kautsar.rahman@gmail.com>
2021-12-16 19:38:32 +0900

    update-layout

--- resources/views/layouts/slime.blade.php
+++ resources/views/layouts/slime.blade.php
@@ -9,6 +9,7 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <style>
+            html {font-size: 14px;}
             body{font-size:14px;color: #2c2c2c;background: #fafafa;font-family: 'Nunito', serif;}
             .container{margin-top:1.4rem; margin-bottom:2rem;}
             .button {margin:0;margin:0;}
