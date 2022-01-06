545ef77e902fcdea6c1fcac88ba09f236ffda493
krahman80 <kautsar.rahman@gmail.com>
2022-01-06 15:35:40 +0900

    add-passport-auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,45 +4,32 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>miligram</title>
+        <title>laravel</title>
         <!-- Google Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-        <!-- CSS Reset -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
-        <!-- Milligram CSS -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <style>
             body{font-size:14px;color: #2c2c2c;background: #fafafa;font-family: 'Nunito', serif;}
             .container{margin-top:1.4rem; margin-bottom:2rem;}
         </style>
     </head>
     <body>
-        <section class="container">
-            <h3 class="title">Typography</h3>
-            <p class="description">CSS3 introduces a few new units, including the <code>rem</code> unit, which stands for <em>"root em"</em>. The <code>rem</code> unit is relative to the font-size of the root element <code>html</code> . That means that we can define a single font size on the root element, and define all <code>rem</code> units to be a percentage of that. The typography has <code>font-size</code> defined in 1.6rem (16px) and <code>line-height</code> in 1.6 (24px). <strong>Milligram</strong> uses the <code>font-family</code><a href="https://www.google.com/fonts/specimen/Roboto" title="Roboto by Christian Robertson" target="_blank" rel="noopener"> Roboto</a>, created by <u>Christian Robertson</u>, and provided by Google.</p>
-            <div class="row">
-              <div class="column">
-                  <div class="box">
-                      <h3>Test</h3>
-                      <p>
-                        CSS3 introduces a few new units, including the rem unit, which stands for "root em". The rem unit is relative to the font-size of the root element html . That means that we can define a single font size on the root element, and define all rem units to be a percentage of that. The typography has font-size defined in 1.6rem (16px) and line-height in 1.6 (24px). Milligram uses the font-family Roboto, created by Christian Robertson, and provided by Google.
-                      </p>
-                  </div>
+      <div class="flex-center position-ref full-height">
 
-              </div>
-              
-              <div class="column">
-                <h3>Test</h3>
-                <p>
-                    CSS3 introduces a few new units, including the rem unit, which stands for "root em". The rem unit is relative to the font-size of the root element html . That means that we can define a single font size on the root element, and define all rem units to be a percentage of that. The typography has font-size defined in 1.6rem (16px) and line-height in 1.6 (24px). Milligram uses the font-family Roboto, created by Christian Robertson, and provided by Google.
-                  </p>
-                <ul><li>Unordered list item 1</li><li>Unordered list item 2</li></ul></div>
+        @if (Route::has('login') && Auth::check())
+            <div class="top-right links">
+                <a href="{{ url('/home') }}">Dashboard</a>
             </div>
+        @elseif (Route::has('login') && !Auth::check())
+            <div class="top-right links">
+                <a href="{{ url('/login') }}">Login</a>
+                <a href="{{ url('/register') }}">Register</a>
+            </div>
+        @endif
+
+        <div class="content">
           
-        </section>
-        <section class="container">
-            <h3 class="title">Typography</h3>
-            <p class="description">CSS3 introduces a few new units, including the <code>rem</code> unit, which stands for <em>"root em"</em>. The <code>rem</code> unit is relative to the font-size of the root element <code>html</code> . That means that we can define a single font size on the root element, and define all <code>rem</code> units to be a percentage of that. The typography has <code>font-size</code> defined in 1.6rem (16px) and <code>line-height</code> in 1.6 (24px). <strong>Milligram</strong> uses the <code>font-family</code><a href="https://www.google.com/fonts/specimen/Roboto" title="Roboto by Christian Robertson" target="_blank" rel="noopener"> Roboto</a>, created by <u>Christian Robertson</u>, and provided by Google.</p>
-        </section>
+        </div>
+    </div>
     </body>
 </html>
