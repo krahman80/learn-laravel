56c3017bb339fe12fdd0f1ef8c5fefeba7db6c59
krahman80 <kautsar.rahman@gmail.com>
2022-01-08 17:34:39 +0900

    add-author-api

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->prefix('v1')->group(function(){
+    Route::get('/user', function(Request $request){
+        return $request->user();
+    });
+
+    // Route::get('/authors','AuthorsController@index');
+    // Route::get('/authors/{author}', 'AuthorsController@show');
+    Route::apiResource('authors','AuthorsController');
 });
